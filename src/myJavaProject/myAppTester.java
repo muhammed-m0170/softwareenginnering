cef908b8c78c8fc0f15933638699f5a6900fbd7b
Muhammed Muhammed <muhmdch7@gmail.com>
2020-05-22 15:45:50 -0700

    New content has been added

---
+++ src/myJavaProject/myAppTester.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package myJavaProject;
+
+import java.util.Scanner;
+
+/**
+ * @author User
+ *
+ */
+public class myAppTester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		int l,b;
+		int a = 0;
+		
+		System.out.println("Enter length");
+		l = input.nextInt();
+		System.out.println("Enter breadth");
+		b = input.nextInt();
+		a = l*b;
+		System.out.println("area : "+ a);
+		System.out.println("Hello Software Engineering World");
+
+	}
+
+}
